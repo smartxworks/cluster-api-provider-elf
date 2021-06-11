0bc69ee451c7a02ce3745d5c27b5501abdc889be
haijianyang <weber.yhj@gmail.com>
2021-06-11 18:55:57 +0800

    Add controller manager

---
+++ pkg/manager/constants.go
@@ -0,0 +1,19 @@
+package manager
+
+import "time"
+
+const (
+	defaultPrefix = "cape-"
+
+	// DefaultSyncPeriod is the default value for the eponymous
+	// manager option.
+	DefaultSyncPeriod = time.Minute * 10
+
+	// DefaultPodName is the default value for the eponymous manager option.
+	DefaultPodName = defaultPrefix + "controller-manager"
+
+	DefaultPodNamespace = defaultPrefix + "system"
+
+	// DefaultLeaderElectionID is the default value for the eponymous manager option.
+	DefaultLeaderElectionID = DefaultPodName + "-runtime"
+)
